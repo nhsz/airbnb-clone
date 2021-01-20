ab9cd0df59b10a460e35255ec94ed8e7ef3773b2
nhsz <nh.quiroz@gmail.com>
2021-01-20 15:02:19 -0300

    feat: add /lib/api module

---
+++ client/src/lib/api/api.ts
@@ -0,0 +1,30 @@
+interface RequestBody<TVariables> {
+  query: string;
+  variables?: TVariables;
+}
+
+interface Error {
+  message: string;
+}
+
+const GRAPHQL_ENDPOINT = '/api';
+
+const api = {
+  fetch: async <TData = any, TVariables = any>(body: RequestBody<TVariables>) => {
+    const res = await fetch(GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch from server.');
+    }
+
+    return res.json() as Promise<{ data: TData; errors: Error[] }>;
+  }
+};
+
+export { api };
