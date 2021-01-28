a2f184557d4db015426a3af27232be21ed977c10
nhsz <nh.quiroz@gmail.com>
2021-01-28 18:16:42 -0300

    feat: React Router setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,39 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
-import { StrictMode } from 'react';
+import { FC, StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { Listings } from './components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { Listings } from './components';
+import { Home, Host, Listing, Listings, NotFound, User } from './pages';
+
+// import './styles/index.css';
 
 const client = new ApolloClient({
   uri: '/api',
   cache: new InMemoryCache()
 });
 
+const App: FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/host' component={Host} />
+        <Route exact path='/listings/:location?' component={Listings} />
+        <Route exact path='/listing/:id' component={Listing} />
+        <Route exact path='/user/:id' component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
 ReactDOM.render(
   <StrictMode>
     <ApolloProvider client={client}>
       <ChakraProvider>
-        <Listings title='Little Airbnb clone' />
+        {/* <Listings title='Little Airbnb clone' /> */}
+        <App />
       </ChakraProvider>
     </ApolloProvider>
   </StrictMode>,
