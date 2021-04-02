dee1e15fe5201c6d12a53e86c67888b3edbfadde
nhsz <nh.quiroz@gmail.com>
2021-04-02 07:28:05 -0300

    chore: add ListingCard component

--- client/src/components/ListingCard/ListingCard.tsx
+++ client/src/components/ListingCard/ListingCard.tsx
@@ -1,46 +1,101 @@
-import { Box, Flex, Icon, Image, Text } from '@chakra-ui/react';
-import { FC } from 'react';
-import { HiStar } from 'react-icons/hi';
+import { Badge, Box, Flex, Icon, Image, Stack, Text } from '@chakra-ui/react';
+import { FC, useState } from 'react';
+import { HiOutlineHeart, HiStar } from 'react-icons/hi';
+import { formatListingPrice } from '../../utils';
 
 interface Props {
-  listing: {
-    id: string;
-    title: string;
-    image: string;
-    address: string;
-    city: string;
-    price: number;
-    numberOfGuests: number;
-    numberOfBeds: number;
-    numberOfBaths: number;
-    rating: number;
-  };
+  id: string;
+  title: string;
+  image: string;
+  address: string;
+  city: string;
+  type: string;
+  price: number;
+  // numberOfGuests: number;
+  // numberOfBeds: number;
+  // numberOfBaths: number;
+  rating: number;
 }
 
-const ListingCard: FC<Props> = ({ listing }) => {
-  const { title, image, address, city, price, rating } = listing;
+const ListingCard: FC<Props> = ({ id, title, image, address, city, type, price, rating }) => {
+  const [fillColor, setFillColor] = useState('gray.600');
 
   return (
-    <Box p='5' maxW='320px' borderWidth='1px'>
-      <Image borderRadius='md' src={image} />
-      <Flex align='baseline' mt={2}>
-        <Text ml={2} textTransform='uppercase' fontSize='sm' fontWeight='bold' color='pink.800'>
-          {city}
+    <Box
+      p='3'
+      maxW='320px'
+      border='1px'
+      borderColor='gray.100'
+      borderRadius='sm'
+      d='flex'
+      flexDirection='column'
+      justifyContent='space-between'
+      _hover={{
+        boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'
+      }}
+    >
+      <Stack>
+        <Box pos='relative'>
+          <Image
+            borderRadius='md'
+            src={image}
+            h={48}
+            w='100%'
+            border='1px'
+            borderColor='gray.100'
+          />
+          <Icon
+            as={HiOutlineHeart}
+            pos='absolute'
+            top={1}
+            right={{ base: 2, md: 3, xl: 2 }}
+            color='gray.300'
+            fill={fillColor}
+            fontSize='2xl'
+            cursor='pointer'
+            onClick={() => setFillColor('red.400')}
+          />
+        </Box>
+
+        <Flex align='baseline' mt={2} mb={4}>
+          <Badge colorScheme={type.toUpperCase() === 'APARTMENT' ? 'pink' : 'green'} mr={2}>
+            {type}
+          </Badge>
+          <Text textTransform='uppercase' fontSize='sm' fontWeight='bold' color='gray.500'>
+            {city}
+          </Text>
+        </Flex>
+
+        <Text lineHeight='short'>{title}</Text>
+        <Text mt={1} fontSize='sm' lineHeight='short' color='gray.500'>
+          {address}
         </Text>
-      </Flex>
-      <Text mt={2} fontSize='xl' fontWeight='semibold' lineHeight='short'>
-        {title}
-      </Text>
-      <Text mt={1} fontSize='sm' lineHeight='short'>
-        {address}
-      </Text>
-      <Text mt={2}>${price}/night</Text>
-      <Flex mt={2} align='center'>
-        <Icon as={HiStar} color='orange.400' />
-        <Text ml={1} fontSize='sm'>
-          <b>{rating}</b>
-        </Text>
-      </Flex>
+      </Stack>
+
+      <Stack mt={6}>
+        <Flex justify='space-between'>
+          <Flex align='center'>
+            <Icon as={HiStar} color='red.500' />
+            <Text ml={1} fontSize='xs'>
+              <b>{rating}</b>
+            </Text>
+          </Flex>
+
+          <Flex align='center'>
+            <Text>
+              <span style={{ fontSize: '1.125rem', fontWeight: 500 }}>
+                {`$${formatListingPrice(price)}`}
+              </span>
+            </Text>
+            <Text fontSize='2xl' fontWeight='hairline' color='gray.500'>
+              /
+            </Text>
+            <Text mt={1} fontWeight='light' color='gray.500'>
+              night
+            </Text>
+          </Flex>
+        </Flex>
+      </Stack>
     </Box>
   );
 };
