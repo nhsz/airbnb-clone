b0fbf8cc6751623a1abc95aa3bcdd64d06043b1e
nhsz <nh.quiroz@gmail.com>
2021-04-02 07:29:17 -0300

    chore: update User page

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,5 +2,7 @@ export * from './Header';
 export * from './HomeSkeleton';
 export * from './ListingCard';
 export * from './UI';
+export * from './UserBookings';
+export * from './UserListings';
 export * from './UserMenu';
 export * from './UserProfile';
--- client/src/pages/User/index.tsx
+++ client/src/pages/User/index.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from '@apollo/client';
+import { Stack } from '@chakra-ui/react';
 import { useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { USER, User as UserData, UserVariables } from '../../api/graphql/queries';
 import { LogIn_logIn as Viewer } from '../../api/types';
-import { ErrorBanner, UserProfile, UserProfileSkeleton } from '../../components';
+import { ErrorBanner, UserListings, UserProfile, UserProfileSkeleton } from '../../components';
 
 interface Props {
   viewer: Viewer;
@@ -33,6 +34,11 @@ const User = ({ match, viewer }: Props & RouteComponentProps<MatchParams>) => {
   const user = data ? data.user : null;
   const viewerIsUser = viewer.id === id;
 
+  const userListings = user ? user.listings : null;
+  const userBookings = user ? user.bookings : null;
+
+  console.log({ data });
+
   if (error) {
     return (
       <ErrorBanner
@@ -46,7 +52,37 @@ const User = ({ match, viewer }: Props & RouteComponentProps<MatchParams>) => {
     return <UserProfileSkeleton />;
   }
 
-  return user ? <UserProfile user={user} viewerIsUser={viewerIsUser} /> : null;
+  return user ? (
+    <Stack p={7}>
+      <Stack mb={24}>
+        <UserProfile user={user} viewerIsUser={viewerIsUser} />
+      </Stack>
+
+      <Stack>
+        <Stack>
+          {userListings && (
+            <UserListings
+              userListings={userListings}
+              listingsPage={listingsPage}
+              setListingsPage={setListingsPage}
+              limit={PAGE_LIMIT}
+            />
+          )}
+        </Stack>
+
+        {/* <Stack>
+          {userBookings && (
+            <UserBookings
+              userBookings={userBookings}
+              bookingsPage={bookingsPage}
+              limit={PAGE_LIMIT}
+              setBookingsPage={setBookingsPage}
+            />
+          )}
+        </Stack> */}
+      </Stack>
+    </Stack>
+  ) : null;
 };
 
 export { User };
