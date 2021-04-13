927fe1eb8157243e389d0788ffe2a9b66091141e
nhsz <nh.quiroz@gmail.com>
2021-04-13 20:24:47 -0300

    feat: add listing to Booking resolver

--- server/src/graphql/resolvers/Booking/index.ts
+++ server/src/graphql/resolvers/Booking/index.ts
@@ -1,9 +1,16 @@
 import { IResolvers } from 'apollo-server-express';
-import { Booking } from '../../../lib/types';
+import { Booking, Database, Listing } from '../../../lib/types';
 
 const bookingResolvers: IResolvers = {
   Booking: {
-    id: (booking: Booking): string => String(booking._id)
+    id: (booking: Booking): string => String(booking._id),
+    listing: (
+      booking: Booking,
+      _args: Record<string, unknown>,
+      { db }: { db: Database }
+    ): Promise<Listing | null> => {
+      return db.listings.findOne({ _id: booking.listing });
+    }
   }
 };
 
